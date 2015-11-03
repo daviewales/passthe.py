65eef96f015518e25302d5335df7800301c7adfb
David Wales <daviewales@gmail.com>
2015-11-04 01:25:14 +1100

    Tweak code for prettiness

--- passthe.py
+++ passthe.py
@@ -7,11 +7,12 @@ import binascii
 import base64
 
 home = os.getenv('HOME')
+seed_path = home + '/.PTPie'
 
 site = input('Site: ').encode('utf-8')
 password = getpass.getpass().encode('utf-8')
 
-with open(home + '/.PTPie', 'r') as file:
+with open(seed_path, 'r') as file:
     seed = file.read().encode('utf-8')
 
 new_password = hashlib.pbkdf2_hmac('sha512', site + password, seed, 200000)
